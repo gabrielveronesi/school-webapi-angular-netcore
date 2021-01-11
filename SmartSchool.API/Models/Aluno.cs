40f22535c9d0967575bdafb1ae95cbf2dc248ab4
Gabriel Veronesi <veronesigabriel@live.com>
2021-01-11 17:17:48 -0300

    Adicionando propriedades no Aluno e Professor

--- SmartSchool.API/Models/Aluno.cs
+++ SmartSchool.API/Models/Aluno.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace SmartSchool.API.Models
@@ -5,18 +6,33 @@ namespace SmartSchool.API.Models
     public class Aluno
     {
         public Aluno() { }
-        public Aluno(int id, string nome, string sobrenome, string telefone)
+
+            //paramêtros passado para o Aluno quer dizer que é obrigatorio na hora da criação!
+        public Aluno(int id,
+                     int matricula,
+                     string nome,
+                     string sobrenome,
+                     string telefone,
+                     DateTime dataNasc)
         {
             this.Id = id;
             this.Nome = nome;
             this.Sobrenome = sobrenome;
             this.Telefone = telefone;
+            this.Matricula = matricula;
+            this.DataNasc = dataNasc;
 
         }
         public int Id { get; set; }
+        public int Matricula { get; set; }
         public string Nome { get; set; }
         public string Sobrenome { get; set; }
         public string Telefone { get; set; }
+        public DateTime DataNasc { get; set; }
+        public DateTime DataIni { get; set; } = DateTime.Now;
+        public DateTime? DataFim { get; set; } = null;
+        public bool Ativo { get; set; } = true;
+
 
         public IEnumerable<AlunoDisciplina> AlunosDisciplinas { get; set; }
     }
--- SmartSchool.API/Models/Professor.cs
+++ SmartSchool.API/Models/Professor.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace SmartSchool.API.Models
@@ -5,14 +6,27 @@ namespace SmartSchool.API.Models
     public class Professor
     {
         public Professor() { }
-        public Professor(int id, string nome)
+
+    //paramêtros passado para o Professor quer dizer que é obrigatorio na hora da criação!
+        public Professor(int id,
+                         int registro,
+                         string nome, 
+                         string sobrenome)
         {
             this.Id = id;
             this.Nome = nome;
+            this.Sobrenome = sobrenome;
+            this.Registro = registro;
+
         }
         public int Id { get; set; }
+        public int Registro { get; set; }
         public string Nome { get; set; }
-    
+        public string Sobrenome { get; set; }
+        public string Telefone { get; set; }
+        public DateTime DataNasc { get; set; }
+        public DateTime DataIni { get; set; } = DateTime.Now;
+        public DateTime? DataFim { get; set; } = null;
         public IEnumerable<Disciplina> Disciplinas { get; set; }
     }
 }
