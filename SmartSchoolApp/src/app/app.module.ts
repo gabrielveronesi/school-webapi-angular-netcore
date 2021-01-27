6d5fe3eb96eb226e476503da1e725e13a6f96534
Gabriel Veronesi <57780382+gabrielveronesi@users.noreply.github.com>
2021-01-27 00:30:34 -0300

    Arrumando Bugs do Angular + Voltando para a versão 10.0.0

---
+++ SmartSchoolApp/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { AlunosComponent } from './components/alunos/alunos.component';
+import { ProfessoresComponent } from './components/professores/professores.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { TituloComponent } from './components/shared/titulo/titulo.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+@NgModule({
+  declarations: [
+    AppComponent,
+    AlunosComponent,
+    ProfessoresComponent,
+    PerfilComponent,
+    DashboardComponent,
+    NavComponent,
+    TituloComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgxSpinnerModule,
+    BsDropdownModule.forRoot(),
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      timeOut:3500,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+      progressBar: true,
+      closeButton: true
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
